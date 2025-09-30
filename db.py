f37d9dec7484188079f45781111d978679d0a164
joana de souza <joanasouzasilva.2008@gmail.com>
2025-09-30 16:42:48 -0300

    update na função de conectar ✔

--- db.py
+++ db.py
@@ -8,9 +8,19 @@ import os
 load_dotenv()
 
 params = {
-    "db_name": os.getenv("db_name"),
+    "dbname": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT"),
 }
+
+def conectar():
+    try:
+        conexao = pg.connect(**params)
+        cursor = conexao.cursor()
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexão {erro}")
+        return None, None
+    
