2537c8eed1363bdddcfd998a3e3bf34ce3e0d15a
joana de souza <joanasouzasilva.2008@gmail.com>
2025-10-06 14:15:26 -0300

    função listar alunos

--- crud.py
+++ crud.py
@@ -14,3 +14,20 @@ def criar_aluno(nome, idade):
         finally:
             cursor.close()
             conexao.close()
+
+def listar_alunos():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("SELECT * FROM alunos ORDER BY id")
+            return cursor.fetchall()
+        except Exception as erro: 
+            print(f"erro ao tentar listar alunos: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+lista = listar_alunos()
+print(lista)
+for aluno in lista:
+    print(aluno)
--- db.py
+++ db.py
@@ -19,8 +19,8 @@ def conectar():
     try:
         conexao = pg.connect(**params)
         cursor = conexao.cursor()
+        print("deu certo! ")
         return conexao, cursor
     except Exception as erro:
         print(f"Erro de conexão {erro}")
         return None, None
-    
