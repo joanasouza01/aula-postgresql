68356c3c928fec3583ab3cdb29b57cc831b7b374
joana de souza <joanasouzasilva.2008@gmail.com>
2025-10-06 14:30:34 -0300

    função atualizar idade

--- crud.py
+++ crud.py
@@ -27,7 +27,19 @@ def listar_alunos():
             cursor.close()
             conexao.close()
 
-lista = listar_alunos()
-print(lista)
-for aluno in lista:
-    print(aluno)
+def atualiza_idade(id_aluno, nova_idade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "UPDATE alunos SET idade = %s WHERE id = %s",
+                (nova_idade, id_aluno)
+                )
+            conexao.commit()
+        except Exception as erro: 
+            print(f"erro ao tentar atualizar a idade {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+
